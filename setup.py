434c3b82cfa651c1024b8cbf068a2a6aa9790478
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-05-27 23:51:00 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='xkcdpass',
+    version='1.0',
+    author='Steven Tobin',
+    author_email='steventtobin@gmail.com',
+    url='https://github.com/redacted/XKCD-password-generator',
+    description='Generate secure multiword passwords/passphrases, inspired by XKCD',
+    long_description=open('README.mkd').read(),
+    package_dir={'': 'xkcdpass'},
+    packages=['xkcdpass'],
+    zip_safe=False,
+    license='BSD',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'xkcdpass = xkcdpass.xkcd_password:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
