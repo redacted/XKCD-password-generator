a1e83c610571f1b3ffa411c38eb2e7a7cf2a5142
Ben Finney <ben+python@benfinney.id.au>
2016-03-05 16:16:12 +1100

    Refine set of supported Python versions.

--- setup.py
+++ setup.py
@@ -21,7 +21,12 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Environment :: Console',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
