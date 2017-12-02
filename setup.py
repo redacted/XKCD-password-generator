93e06e45c1b27ee44031c166c0a1f9e22aed0c49
François Freitag <mail+github@franek.fr>
2017-12-02 13:45:57 -0800

    Mark Python 3.6 as supported

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Environment :: Console',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
