e1ef873631e4a0dad6a1865f11e010090ca5f8af
Steven Tobin <steven@steventob.in>
2026-01-11 16:03:32 +0000

    Test fixes

--- tests/test_xkcdpass.py
+++ tests/test_xkcdpass.py
@@ -29,7 +29,7 @@ class XkcdPasswordTests(unittest.TestCase):
             valid_chars='[a-z]')
 
     def test_loadwordfile(self):
-        self.assertEqual(len(self.wordlist_full), 65353)
+        self.assertEqual(len(self.wordlist_full), 5670)
 
     def test_regex(self):
         self.assertNotIn("__$$$__", self.wordlist_small)
